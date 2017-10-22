70bee1e66baf7dbed93f171f89ec5aefd67ce5d2
Alex J <alexjovermorales@gmail.com>
2017-10-22 13:34:51 +0200

    fix: fix everything

--- src/utils.ts
+++ src/utils.ts
@@ -10,16 +10,16 @@ export const logger = console as Logger
  */
 export function compareSets(set1: Set<any>, set2: Set<any>) {
   if (set1.size !== set2.size) {
-    return true
+    return false
   }
   const set1arr = Array.from(set1)
   const set2arr = Array.from(set2)
 
   for (let i = 0; i < set1arr.length; i++) {
     if (set1arr[i] !== set2arr[i]) {
-      return true
+      return false
     }
   }
 
-  return false
+  return true
 }
